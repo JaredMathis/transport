a28ded6bd57af1dcc781acbec9c7252b8a08186a
JaredMathis <jared.mathis@native.consulting>
2023-02-07 11:53:36 -0500

    function_auto component_top

--- src/component/top.mjs
+++ src/component/top.mjs
@@ -1,4 +1,8 @@
+import { element } from './../../node_modules/m00/src/element.mjs';
+import { element_style } from './../../node_modules/m00/src/element/style.mjs';
 import { element_with_text } from './../../node_modules/m00/src/element/with/text.mjs';
 export function component_top(parent) {
-    element_with_text(parent, 'div', 'hello');
+    let container = element(parent);
+    element_style(container, { backgroundColor: 'black ' });
+    element_with_text(container, 'div', 'hello');
 }
