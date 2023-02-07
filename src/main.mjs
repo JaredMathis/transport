0058d21e789232997ed11011b13ee6d5d62762db
JaredMathis <jared.mathis@native.consulting>
2023-02-07 13:06:53 -0500

    function_auto main

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,13 @@
+import { element_style } from './../node_modules/m00/src/element/style.mjs';
+import { element_style_default } from './../node_modules/m00/src/element/style/default.mjs';
+import { defaults_set } from './../node_modules/m00/src/defaults/set.mjs';
+import { element } from './../node_modules/m00/src/element.mjs';
 import { element_style } from 'm00/src/element/style.mjs';
+import { element_style_default } from 'm00/src/element/style/default.mjs';
 import { component_top } from './component/top.mjs';
 export function main() {
+    defaults_set(element, { fontFamily: 'Sans-Serif' });
     const parent = document.body;
-    element_style(parent, { margin: 0 })
+    element_style(parent, { margin: 0 });
     component_top(parent);
 }
