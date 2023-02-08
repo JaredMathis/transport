494e02c0e3cf37ca890001db2867f3cbc920e2b4
JaredMathis <jared.mathis@native.consulting>
2023-02-07 23:26:16 -0500

    function_auto main

--- src/main.mjs
+++ src/main.mjs
@@ -1,18 +1,19 @@
+import { element_attribute_merge } from './../node_modules/m00/src/element/attribute/merge.mjs';
 import { element_style } from './../node_modules/m00/src/element/style.mjs';
 import { defaults_set } from './../node_modules/m00/src/defaults/set.mjs';
 import { element } from './../node_modules/m00/src/element.mjs';
 import { component_top } from './component/top.mjs';
 export function main() {
-    defaults_set(element.name, { 
+    defaults_set(element.name, {
         fontFamily: 'Sans-Serif',
-        fontSize: '2.8vh',
-     });
+        fontSize: '2.8vh'
+    });
     const parent = document.body;
     element_style(parent, { margin: 0 });
     component_top(parent);
     let s = element(parent, 'script');
-    merge(s, {
-        src: "https://kit.fontawesome.com/9fc689c523.js",
-        crossorigin: "anonymous",
-    })
+    element_attribute_merge(s, {
+        src: 'https://kit.fontawesome.com/9fc689c523.js',
+        crossorigin: 'anonymous'
+    });
 }
