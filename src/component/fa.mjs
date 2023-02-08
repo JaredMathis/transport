8660f8329d41e5e2dc101129199229ee9442fcc8
JaredMathis <jared.mathis@native.consulting>
2023-02-08 00:09:27 -0500

    function_auto component_fa

--- src/component/fa.mjs
+++ src/component/fa.mjs
@@ -1,2 +1,7 @@
-export function component_fa() {
+import { element } from './../../node_modules/m00/src/element.mjs';
+import { element_attribute_merge } from './../../node_modules/m00/src/element/attribute/merge.mjs';
+export function component_fa(parent, name) {
+    let result = element(parent, 'i');
+    element_attribute_merge(result, { class: 'fa-solid fa-' + name });
+    return result;
 }
