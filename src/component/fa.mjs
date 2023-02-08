a38ab33eaaa374d1adbb044ed34477bd5e459ead
JaredMathis <jared.mathis@native.consulting>
2023-02-08 00:16:27 -0500

    function_auto component_fa

--- src/component/fa.mjs
+++ src/component/fa.mjs
@@ -1,10 +1,9 @@
+import { element_style } from './../../node_modules/m00/src/element/style.mjs';
 import { element } from './../../node_modules/m00/src/element.mjs';
 import { element_attribute_merge } from './../../node_modules/m00/src/element/attribute/merge.mjs';
 export function component_fa(parent, name) {
     let result = element(parent, 'i');
-    element_attribute_merge(result, {
-        class: 'fa-solid fa-' + name,
-        fontFamily: null
-    });
+    element_attribute_merge(result, { class: 'fa-solid fa-' + name });
+    element_style(e, { fontFamily: null });
     return result;
 }
